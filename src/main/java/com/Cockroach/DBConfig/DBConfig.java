1d290acf9acbbae5ea4a62b23bd95b8400830c14
Tilak Singh <Tilak.Singh@colorado.edu>
2023-11-22 19:50:35 -0700

    Singleton pattern

--- src/main/java/com/Cockroach/DBConfig/DBConfig.java
+++ src/main/java/com/Cockroach/DBConfig/DBConfig.java
@@ -2,6 +2,8 @@ package com.Cockroach.DBConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
@@ -9,8 +11,14 @@ import javax.sql.DataSource;
 @Configuration
 public class DBConfig {
 
+    @Lazy
+    @Scope("singleton") // Singleton pattern
     @Bean
     public DataSource dataSource() {
+        return createDataSource();
+    }
+
+    private DataSource createDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl("jdbc:postgresql://newest-ant-12734.7tt.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full");
